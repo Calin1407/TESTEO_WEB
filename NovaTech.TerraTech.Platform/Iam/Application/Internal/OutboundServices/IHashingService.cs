a1deaea9c1572c6ba7668b42a9a69900ddfe9c1b
Calin <reppollio@gmail.com>
2026-06-05 00:24:35 -0500

    feat(hashing): added hashing service interface in application outbound service.

---
+++ NovaTech.TerraTech.Platform/Iam/Application/Internal/OutboundServices/IHashingService.cs
@@ -0,0 +1,35 @@
+namespace NovaTech.TerraTech.Platform.Iam.application.Internal.OutboundServices;
+
+/// <summary>
+///     The hashing service interface.
+/// </summary>
+/// <remarks>
+///     This interface is used to hash and verify password.
+/// </remarks>
+public interface IHashingService
+{
+    /// <summary>
+    ///     Hash a password.
+    /// </summary>
+    /// <param name="password">
+    ///     The password to hash.
+    /// </param>
+    /// <returns>
+    ///     The hashed password.
+    /// </returns>
+    string HashPassword(string password);
+    
+    /// <summary>
+    ///     Verify a password.
+    /// </summary>
+    /// <param name="password">
+    ///     The password to verify.
+    /// </param>
+    /// <param name="passwordHash">
+    ///     The password hash to verify against.
+    /// </param>
+    /// <returns>
+    ///     True if the password is valid, in otherwise false.
+    /// </returns>
+    bool VerifyPassword(string password, string passwordHash);
+}
