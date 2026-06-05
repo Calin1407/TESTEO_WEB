78bc66476aa7ebf0db9ed45c27296361d6693769
Calin <reppollio@gmail.com>
2026-06-04 21:00:53 -0500

    feat(iam): updated user aggregate and added in repository cancellation token.

--- NovaTech.TerraTech.Platform/Iam/Domain/Model/Aggregates/User.cs
+++ NovaTech.TerraTech.Platform/Iam/Domain/Model/Aggregates/User.cs
@@ -21,12 +21,30 @@ public partial class User(Email emailAddress, string passwordHash)
     [JsonIgnore]
     public string PasswordHash { get; private set; } = passwordHash;
 
+    /// <summary>
+    ///     Update Email
+    /// </summary>
+    /// <param name="newEmail">
+    ///     The user new Email
+    /// </param>
+    /// <returns>
+    ///     The user's Email updated
+    /// </returns>
     public User UpdateEmail(Email newEmail)
     {
         EmailAddress = newEmail;
         return this;
     }
 
+    /// <summary>
+    ///     Update password hash.
+    /// </summary>
+    /// <param name="newPasswordHash">
+    ///     The user new password.
+    /// </param>
+    /// <returns>
+    ///     The user's password updated.
+    /// </returns>
     public User UpdatePasswordHash(string newPasswordHash)
     {
         PasswordHash = newPasswordHash;
