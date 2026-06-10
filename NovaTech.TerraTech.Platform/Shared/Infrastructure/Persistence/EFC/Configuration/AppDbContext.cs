acf91c0fb2c77d16ec3ec0640e38574085edc2d1
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-09 19:57:54 -0500

    refactor(stock): appdbcontext by removing commented-out configurations.

--- NovaTech.TerraTech.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ NovaTech.TerraTech.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,9 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using NovaTech.TerraTech.Platform.NotificationManagement.Domain.Model.Aggregates;
-using NovaTech.TerraTech.Platform.ProfileManagement.Domain.Model.Aggregates;
-using NovaTech.TerraTech.Platform.StockManagement.Domain.Model.Aggregates;
 using NovaTech.TerraTech.Platform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using NovaTech.TerraTech.Platform.Shared.Infrastructure.Persistence.EFC.Interceptors;
+using Microsoft.EntityFrameworkCore;
 
 namespace NovaTech.TerraTech.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
 
@@ -30,12 +27,6 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         
         // Stock Management Context
         builder.ApplyStockConfiguration();
-
-        // Profile Management Context (cuando lo tengas)
-        // builder.ApplyProfileConfiguration();
-
-        // Monitoring Context (cuando lo tengas)
-        // builder.ApplyMonitoringConfiguration();
 
         // General Naming Convention for the database objects
         builder.UseSnakeCaseNamingConvention();
